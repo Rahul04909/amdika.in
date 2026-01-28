ca85f85d3e35cd6d89235b83d91ceb4b0a00ff82
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-01-29 00:22:16 +0530

    updated header menu

--- includes/header.php
+++ includes/header.php
@@ -150,7 +150,7 @@ if (strpos($current_script, '/user/') !== false) {
                                     <!-- Menu Links -->
                                     <div class="main-nav">
                                         <a href="#" class="nav-link ps-lg-0">Home</a>
-                                        <a href="#" class="nav-link">Shop</a>
+                                        <a href="../../products.php" class="nav-link">Shop</a>
                                         <a href="#" class="nav-link">About us</a>
                                         <a href="#" class="nav-link">Contact</a>
                                     </div>
