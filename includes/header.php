28e29ecb165aa82b6aaca6105e93ee07b08e7cf2
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-02 10:33:57 +0530

    updated header menu

--- includes/header.php
+++ includes/header.php
@@ -151,9 +151,9 @@ if (strpos($current_script, '/user/') !== false) {
                                 <div class="d-flex flex-column flex-lg-row align-items-lg-center justify-content-between w-100">
                                     <!-- Menu Links -->
                                     <div class="main-nav">
-                                        <a href="#" class="nav-link ps-lg-0">Home</a>
+                                        <a href="https://www.amdika.in" class="nav-link ps-lg-0">Home</a>
                                         <a href="../../products.php" class="nav-link">Shop</a>
-                                        <a href="#" class="nav-link">About us</a>
+                                        <a href="../../pages/about-us/index.php" class="nav-link">About us</a>
                                         <a href="#" class="nav-link">Contact</a>
                                     </div>
                                 </div>
