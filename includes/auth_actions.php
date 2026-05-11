53ba9fd4541746fd7c7e9522f731fd89d7892c6d
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-05-11 15:08:43 +0530

    updated otp ssl issue

--- includes/auth_actions.php
+++ includes/auth_actions.php
@@ -30,6 +30,8 @@ function sendOTP($mobile, $otp) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
     $response = curl_exec($ch);
     $err = curl_error($ch);
     curl_close($ch);
