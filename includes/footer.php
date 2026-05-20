083a62bce02a365a128b9452589b093ff0f79920
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-05-20 13:14:47 +0530

    fixed social icons jumping/scrolling to top of page when clicked

--- includes/footer.php
+++ includes/footer.php
@@ -303,11 +303,11 @@
                 <span class="footer-heading">Download Our App</span>
                 <span class="footer-heading" style="font-size: 12px;">Coming Soon</span>
                 <div class="d-flex gap-2">
-                    <a href="#">
+                    <a href="javascript:void(0)">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Download_on_the_App_Store_Badge.svg"
                             alt="App Store" style="height:35px;">
                     </a>
-                    <a href="#">
+                    <a href="javascript:void(0)">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
                             alt="Google Play" style="height:35px;">
                     </a>
@@ -483,9 +483,9 @@
                     <div class="fw-bold mb-2 text-secondary" style="font-size: 13px;">Like what you're seeing? Follow us
                         for more.</div>
                     <div class="social-icons">
-                        <a href="#" class="fb"><i class="fa-brands fa-facebook-f"></i></a>
-                        <a href="#" class="insta"><i class="fa-brands fa-instagram"></i></a>
-                        <a href="#" class="x">
+                        <a href="javascript:void(0)" class="fb"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="javascript:void(0)" class="insta"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="javascript:void(0)" class="x">
                             <svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" viewBox="0 0 512 512"
                                 style="fill: currentColor;">
                                 <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
@@ -493,8 +493,8 @@
                                     d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
                             </svg>
                         </a>
-                        <a href="#" class="yt"><i class="fa-brands fa-youtube"></i></a>
-                        <a href="#" class="in"><i class="fa-brands fa-linkedin-in"></i></a>
+                        <a href="javascript:void(0)" class="yt"><i class="fa-brands fa-youtube"></i></a>
+                        <a href="javascript:void(0)" class="in"><i class="fa-brands fa-linkedin-in"></i></a>
                     </div>
                 </div>
             </div>
